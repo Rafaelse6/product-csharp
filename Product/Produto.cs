0339a9ddbb3721b014182c03fd5d7cb07533ad2d
Rafael Santos <rafa.05125@gmail.com>
2023-04-19 11:47:45 -0300

    Add and remove product

--- Product/Produto.cs
+++ Product/Produto.cs
@@ -17,6 +17,16 @@ namespace Product
             return Preco * Quantidade;
         }
 
+        public void AdicionarProdutos(int quantidade)
+        {
+            Quantidade += quantidade;
+        }
+
+        public void RemoverProdutos(int quantidade)
+        {
+            Quantidade -= quantidade;
+        }
+
         public override string ToString()
         {
             return Nome + ", $ " + Preco.ToString("F2", CultureInfo.InvariantCulture)
