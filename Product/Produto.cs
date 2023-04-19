811d65dff3e46f23df125ae4888aa4163a838532
Rafael Santos <rafa.05125@gmail.com>
2023-04-19 11:39:33 -0300

    ToString

--- Product/Produto.cs
+++ Product/Produto.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Globalization;
 
 namespace Product
 {
@@ -14,6 +15,15 @@ namespace Product
         public double ValorTotalEmEstoque()
         {
             return Preco * Quantidade;
+        }
+
+        public override string ToString()
+        {
+            return Nome + ", $ " + Preco.ToString("F2", CultureInfo.InvariantCulture)
+            + ", "
+            + Quantidade
+            + " unidades, Total: $"
+            + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
         }
     }
 }
