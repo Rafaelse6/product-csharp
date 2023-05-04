435a1550d26ae6855b8932f31bf60ed1dc26e767
Rafael Santos <rafa.05125@gmail.com>
2023-05-03 21:57:23 -0300

    Encapsulation - manual implementation

--- Product/Produto.cs
+++ Product/Produto.cs
@@ -7,48 +7,68 @@ namespace Product
 {
     class Produto
     {
-        public string Nome;
-        public double Preco;
+        private string _nome;
+        private double _preco;
 
-        public int Quantidade;
+        private int _quantidade;
 
 
         public Produto()
         {
-            Quantidade = 10;
+            _quantidade = 10;
         }
 
-        public Produto(string nome, double preco) : this()
+        public Produto(string nome, double preco, int quantidade) : this()
         {
-            Nome = nome;
-            Preco = preco;
+            _nome = nome;
+            _preco = preco;
+            _quantidade = quantidade;
         }
 
-        public Produto(string nome, double preco, int quantidade) : this(nome, preco)
+        public string GetNome()
         {
+            return _nome;
+        }
 
+        public void SetNome(string nome)
+        {
+            if (nome != null && nome.Length > 1)
+            {
+                _nome = nome;
+            }
+
+        }
+
+        public double GetPreco()
+        {
+            return _preco;
+        }
+
+        public int GetQuantidade()
+        {
+            return _quantidade;
         }
 
         public double ValorTotalEmEstoque()
         {
-            return Preco * Quantidade;
+            return _preco * _quantidade;
         }
 
         public void AdicionarProdutos(int quantidade)
         {
-            Quantidade += quantidade;
+            _quantidade += quantidade;
         }
 
         public void RemoverProdutos(int quantidade)
         {
-            Quantidade -= quantidade;
+            _quantidade -= quantidade;
         }
 
         public override string ToString()
         {
-            return Nome + ", $ " + Preco.ToString("F2", CultureInfo.InvariantCulture)
+            return _nome + ", $ " + _preco.ToString("F2", CultureInfo.InvariantCulture)
             + ", "
-            + Quantidade
+            + _quantidade
             + " unidades, Total: $"
             + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
         }
--- Product/Program.cs
+++ Product/Program.cs
@@ -7,46 +7,10 @@ namespace Product
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Entre os dados do produto: ");
-
-            Console.Write("Nome: ");
-            string nome = Console.ReadLine();
-
-            Console.Write("Preço: ");
-            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-
-            /*Console.Write("Quantidade no estoque:");
-            int quantidade = int.Parse(Console.ReadLine());*/
-
-            Produto p = new Produto(nome, preco);
-
-            Produto p2 = new Produto();
-
-            Produto p3 = new Produto
-            {
-                Nome = "TV",
-                Preco = 500.00,
-                Quantidade = 20
-            };
-
-            Console.WriteLine();
-            Console.WriteLine("Dados do produto: " + p);
-
-            Console.WriteLine();
-            Console.Write("Digite o número de produtos a ser adicionado ao estoque: ");
-            int qte = int.Parse(Console.ReadLine());
-            p.AdicionarProdutos(qte);
-
-            Console.WriteLine();
-            Console.WriteLine("Dados atualizados: " + p);
-
-            Console.WriteLine();
-            Console.Write("Digite o número de produtos a ser removido do estoque: ");
-            qte = int.Parse(Console.ReadLine());
-            p.RemoverProdutos(qte);
-
-            Console.WriteLine();
-            Console.WriteLine("Dados atualizados: " + p);
+            Produto p = new Produto("TV", 500.0, 10);
+            p.SetNome("T");
+            Console.WriteLine(p.GetNome());
+            Console.WriteLine(p.GetPreco());
         }
     }
 }
